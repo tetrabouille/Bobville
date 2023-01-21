f21155582a81958f3f4acd6f4ad2ebac2dd0770e
Thibaut MANDALLENA <tmandallena@squad.fr>
2023-01-21 11:34:12 +0100

    improve bob movement

--- Scripts/BobController.cs
+++ Scripts/BobController.cs
@@ -54,16 +54,23 @@ public class BobController : MonoBehaviour
             transform.position.y,
             currentPoint.transform.position.z - transform.position.z
         );
-        Quaternion rotationTarget = Quaternion.LookRotation(target, Vector3.up);
+
+        Vector3 smoothTarget = Vector3.RotateTowards(
+            transform.forward,
+            target,
+            Time.deltaTime * ROTATION_SPEED,
+            0
+        );
 
         if (Vector3.Dot(Vector3.Normalize(target), transform.TransformDirection(Vector3.forward)) >= 1 - ROTATION_EPSILON)
         {
-            transform.rotation = rotationTarget;
+            transform.rotation = Quaternion.LookRotation(target);
             state = BobState.Walking;
             return;
         }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotationTarget, Time.deltaTime * ROTATION_SPEED);
+        Quaternion rotationTarget =
+        transform.rotation = Quaternion.LookRotation(smoothTarget);
     }
 
     void Walk(PointController currentPoint)
@@ -76,7 +83,6 @@ public class BobController : MonoBehaviour
         Debug.Log(Vector3.Distance(transform.position, target));
         if (Vector3.Distance(transform.position, target) <= POINT_EPSILON)
         {
-            transform.position = target;
             state = BobState.Turning;
             pathIndex++;
             return;
